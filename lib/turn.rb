66d5467631ccbc0a699afcc9616f23926da7660c
DJ Taylor <dtaylor88@gmail.com>
2021-05-28 06:28:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,4 +35,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.to_i
   input_to_index(input)
+  if valid_move?(board, index) == true
+    move(board, index, value="X")
+    display_board(board)
+  else
+    turn(board)
+  end
 end
